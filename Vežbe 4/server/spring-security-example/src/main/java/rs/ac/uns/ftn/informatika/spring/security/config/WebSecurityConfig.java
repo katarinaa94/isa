91a72115c8ef14019805d336387ad89e961782d4
stojkovm <stojkovm@uns.ac.rs>
2019-11-16 21:14:29 +0100

    Updated spring-security

--- Vežbe 4/server/spring-security-example/src/main/java/rs/ac/uns/ftn/informatika/spring/security/config/WebSecurityConfig.java
+++ Vežbe 4/server/spring-security-example/src/main/java/rs/ac/uns/ftn/informatika/spring/security/config/WebSecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -22,7 +21,6 @@ import rs.ac.uns.ftn.informatika.spring.security.security.auth.TokenAuthenticati
 import rs.ac.uns.ftn.informatika.spring.security.service.impl.CustomUserDetailsService;
 
 @Configuration
-@EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
