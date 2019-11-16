61c2d087f1e0dd6a0bf7298d7fee2ab6aed6a46d
stojkovm <stojkovm@uns.ac.rs>
2019-11-16 21:12:39 +0100

    Uploaded cache-example

---
+++ Vežbe 5/cache-example/src/main/java/rs/ac/uns/ftn/informatika/cache/domain/Product.java
@@ -0,0 +1,54 @@
+package rs.ac.uns.ftn.informatika.cache.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+
+@Entity
+/*
+ * Primer upita koji se specificira nad samom klasom koja predstavlja tabelu u bazi
+ */
+@NamedQuery(name="Product.findByPrice", query="select name, origin, price from Product p where p.price=?1")
+public class Product implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(name="name")
+	private String name;
+	@Column(name="origin")
+	private String origin;
+	@Column(name="price")
+	private Long price;
+	
+	public String getOrigin() {
+		return origin;
+	}
+	public void setOrigin(String origin) {
+		this.origin = origin;
+	}
+	public long getPrice() {
+		return price;
+	}
+	public void setPrice(long price) {
+		this.price = price;
+	}	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}	
+}
