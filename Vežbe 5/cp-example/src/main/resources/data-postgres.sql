2da1b8921e67ec3edb748e0de988d565c9c7effa
stojkovm <stojkovm@uns.ac.rs>
2020-04-18 18:00:31 +0200

    Cleanup

--- Vežbe 5/cp-example/src/main/resources/data-postgres.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO product(name,origin,price) VALUES ('Mi Band 2','China', 400);
-INSERT INTO product(name,origin,price) VALUES ('Iphone 7','USA', 500);
-INSERT INTO product(name,origin,price) VALUES ('Iphone 3','USA', 100);
