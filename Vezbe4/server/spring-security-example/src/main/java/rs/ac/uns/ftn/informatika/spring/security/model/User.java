2e03ebc3bd8c2f2ad9752994be57455fb116f542
stojkovm <stojkovm@uns.ac.rs>
2020-05-01 15:26:02 +0200

    spring-security-example update

--- Vezbe4/server/spring-security-example/src/main/java/rs/ac/uns/ftn/informatika/spring/security/model/User.java
+++ Vezbe4/server/spring-security-example/src/main/java/rs/ac/uns/ftn/informatika/spring/security/model/User.java
@@ -5,7 +5,6 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -57,7 +56,7 @@ public class User implements UserDetails {
     @Column(name = "last_password_reset_date")
     private Timestamp lastPasswordResetDate;
 
-    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "user_authority",
             joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
             inverseJoinColumns = @JoinColumn(name = "authority_id", referencedColumnName = "id"))
